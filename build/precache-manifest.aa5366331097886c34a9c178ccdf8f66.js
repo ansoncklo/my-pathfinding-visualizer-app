59609d3b9e1a036d030065aa9872a36341999a67
Anson Lo <92462730+ansoncklo@users.noreply.github.com>
2021-10-25 01:09:53 +0800

    Upload my app

---
+++ build/precache-manifest.aa5366331097886c34a9c178ccdf8f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc88fa9312db83c91892a707979bbfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886f569c01c06e926fa9",
+    "url": "/static/css/main.a0ab3751.chunk.css"
+  },
+  {
+    "revision": "a630968cba511403d833",
+    "url": "/static/js/2.8e683c52.chunk.js"
+  },
+  {
+    "revision": "886f569c01c06e926fa9",
+    "url": "/static/js/main.e1ac0040.chunk.js"
+  },
+  {
+    "revision": "512a4f888a379d4d7645",
+    "url": "/static/js/runtime-main.1638fc71.js"
+  }
+]);
